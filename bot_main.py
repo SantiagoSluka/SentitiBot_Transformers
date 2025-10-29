8d2843d43e85e4b80a6e88695bc2aaee687c2e23
Gael <gaelneo18@gmail.com>
2025-10-28 23:20:05 -0300

    subo api, y json

---
+++ bot_main.py
@@ -0,0 +1,23 @@
+import telebot  
+import requests
+import json
+import os
+from groq import Groq
+import base64
+from dotenv import load_dotenv
+#Para api de groq, telegram y a futuro qwem si llegamos
+load_dotenv()
+
+TOKEN_BOT_TELEGRAM = os.getenv('TELEGRAM_BOT_TOKEN')
+CLAVE_API_GROQ = os.getenv('GROQ_API_KEY')
+DATASET_PATH = 'emociones.json'
+
+bot = telebot.TeleBot(TOKEN_BOT_TELEGRAM)
+cliente_groq = Groq(api_key=CLAVE_API_GROQ)
+
+def cargar_dataset():
+	try:
+		with open(DATASET_PATH, 'r', encoding='utf-8') as f:
+			return json.load(f)
+	except Exception:
+		return []
